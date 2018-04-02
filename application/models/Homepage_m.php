2b1a9b0ad60d6542b31c7d25be4351e1ca4ff1ae
admunidayan <35984858+admunidayan@users.noreply.github.com>
2018-04-02 12:08:07 +0700

    update

--- application/models/Homepage_m.php
+++ application/models/Homepage_m.php
@@ -13,14 +13,14 @@ class Homepage_m extends CI_Model
 		$query = $this->db->get('laman');
 		return $query->result();
 	}
-	public function ceksubpage($id){
-		$this->db->where('id_laman', $id);
-		$query = $this->db->get('laman');
+	public function cek_email($data){
+		$this->db->order_by('email','asc');
+		$this->db->where('email', $data);
+		$query = $this->db->get('users');
 		return $query->row();
 	}
 	public function subpage($id){
 		$this->db->where('s_laman', $id);
-		$this->db->where_not_in('s_laman', 0);
 		$query = $this->db->get('laman');
 		return $query->result();
 	}
@@ -28,21 +28,8 @@ class Homepage_m extends CI_Model
 		$query = $this->db->get('kategori');
 		return $query->result();
 	}
-	public function kadok(){
-		$query = $this->db->get('kategori_dokumen');
-		return $query->result();
-	}
-	// Indek artikel
-	public function get_all_artikel($sampai,$dari,$cari){
-		if (!empty($cari)) {
-			$this->db->like('nomor_dokumen',$cari);
-		}
-		$this->db->order_by('id_artikel','desc');
-		$query = $this->db->get('artikel',$sampai,$dari);
-		return $query->result();
-	}
 	// index dokumen
-	function jumlah_doc($string,$tahun,$kat){
+	function jumlah_doc($string,$tahun,$kat,$ten){
 		if (!empty($string)) {
 			$this->db->like('nomor_dokumen',$string);
 		}
@@ -52,9 +39,12 @@ class Homepage_m extends CI_Model
 		if (!empty($kat)) {
 			$this->db->where('id_kategori',$kat);
 		}
+		if (!empty($ten)) {
+			$this->db->like('deskripsi_dokumen',$ten);
+		}
 		return $this->db->get('dokumen')->num_rows();
 	}
-	public function searcing_data($sampai,$dari,$cari,$tahun,$kat){
+	public function searcing_data($sampai,$dari,$cari,$tahun,$kat,$ten){
 		if (!empty($cari)) {
 			$this->db->like('nomor_dokumen',$cari);
 		}
@@ -64,46 +54,12 @@ class Homepage_m extends CI_Model
 		if (!empty($kat)) {
 			$this->db->where('id_kategori',$kat);
 		}
-		$this->db->order_by('id_dokumen','desc');
-		$query = $this->db->get('dokumen',$sampai,$dari);
-		return $query->result();
-	}
-	// index dokumen
-	function jumlah_dokumen($string){
-		if (!empty($string)) {
-			$this->db->like('nomor_dokumen',$string);
-		}
-		return $this->db->get('dokumen')->num_rows();
-	}
-	public function searcing_data_doc($sampai,$dari,$cari){
-		if (!empty($cari)) {
-			$this->db->like('nomor_dokumen',$cari);
+		if (!empty($ten)) {
+			$this->db->like('deskripsi_dokumen',$ten);
 		}
 		$this->db->order_by('id_dokumen','desc');
 		$query = $this->db->get('dokumen',$sampai,$dari);
 		return $query->result();
-	}
-	// kategori dokumen
-	function jumlah_kadok($string){
-		if (!empty($string)) {
-			$this->db->like('nama_kategori_dokumen',$string);
-		}
-		$this->db->where('id_kategori',$string);
-		return $this->db->get('dokumen')->num_rows();
-	}
-	public function searcing_data_kategori_dokumen($sampai,$dari,$alias,$cari){
-		if (!empty($cari)) {
-			$this->db->like('nama_kategori_dokumen',$cari);
-		}
-		$this->db->where('id_kategori',$alias);
-		$this->db->order_by('id_dokumen','desc');
-		$query = $this->db->get('dokumen',$sampai,$dari);
-		return $query->result();
-	}
-	public function idkadok($alias){
-		$this->db->where('alias_kategori_dokumen',$alias);
-		$query = $this->db->get('kategori_dokumen');
-		return $query->row();
 	}
 	// laman
 	public function get_laman($alias){
@@ -121,6 +77,10 @@ class Homepage_m extends CI_Model
 	function jumlah_doc_kat($id){		
 		$this->db->where('id_kategori',$id);
 		return $this->db->get('dokumen')->num_rows();
+	}
+	function jml_pilih($id){		
+		$this->db->where('pilihan',$id);
+		return $this->db->get('pilihan')->num_rows();
 	}
 	public function searcing_data_kat($sampai,$dari,$id){
 		$this->db->where('id_kategori',$id);
@@ -159,15 +119,14 @@ class Homepage_m extends CI_Model
 		$query = $this->db->get('dokumen',8,0);
 		return $query->result();
 	}
+	// baru
+	public function pimpinan(){
+		$query = $this->db->get('pimpinan_pt');
+		return $query->result();
+	}
 	public function linkgambar(){
 		$this->db->where('id_kategori_link',1);
 		$query = $this->db->get('link');
-		return $query->result();
-	}
-	public function info(){
-		$this->db->order_by('id_info_kampus','desc');
-		$this->db->limit(0,5);
-		$query = $this->db->get('info_kampus');
 		return $query->result();
 	}
 	public function linkbykateg(){
@@ -180,5 +139,148 @@ class Homepage_m extends CI_Model
 		$this->db->where('id_kategori_link',$id);
 		$query = $this->db->get('link');
 		return $query->result();
+	}
+	// calon peserta
+	function input_data_calon($data){
+		$this->db->insert('calon_mhs', $data);
+	}
+	public function last_id(){
+		$this->db->order_by('id_mhs','desc');
+		$query = $this->db->get('calon_mhs');
+		return $query->row();
+	}
+	public function data_calon_peserta($id){
+		$this->db->where('id_mhs',$id);
+		$query = $this->db->get('calon_mhs');
+		return $query->row();
+	}
+	public function listagama(){
+		$query = $this->db->get('agama');
+		return $query->result();
+	}
+	public function dttgl(){
+		$query = $this->db->get('jenis_tinggal');
+		return $query->result();
+	}
+	public function pekerjaan(){
+		$query = $this->db->get('pekerjaan');
+		return $query->result();
+	}
+	public function penghasilan(){
+		$query = $this->db->get('penghasilan');
+		return $query->result();
+	}
+	public function pendidikan(){
+		$query = $this->db->get('jenjang_pend');
+		return $query->result();
+	}
+	public function wilayah(){
+		$this->db->where('id_level_wil', 1);
+		$this->db->order_by('nm_wil', 'asc');
+		$query = $this->db->get('data_wilayah');
+		return $query->result();
+	}
+	function input_form_2($data,$id){
+		$this->db->where('id_mhs',$id);
+		$this->db->update('calon_mhs', $data);
+	}
+	function edituser($data,$id){
+		$this->db->where('id',$id);
+		$this->db->update('users', $data);
+	}
+	function upload_img_konfirmasi($data,$id){
+		$this->db->where('id_mhs',$id);
+		$this->db->update('calon_mhs', $data);
+	}
+	function update_data_calon($id,$data){
+		$this->db->where('id_mhs',$id);
+		$this->db->update('calon_mhs', $data);
+	}
+	public function pilih_jurusan1($tipe){
+		if ($tipe !== '3') {
+			$this->db->where('type',$tipe);
+		}
+		$this->db->where('grup',1);
+		$this->db->join('jenjang_pend', 'jenjang_pend.id_jenjang_pend = prodi.id_jenjang_pend');
+		$this->db->order_by('nama_prodi', 'asc');
+		$query = $this->db->get('prodi');
+		return $query->result();
+	}
+	public function pilih_jurusan2($tipe){
+		if ($tipe !== '3') {
+			$this->db->where('type',$tipe);
+		}
+		$this->db->where('grup',2);
+		$this->db->join('jenjang_pend', 'jenjang_pend.id_jenjang_pend = prodi.id_jenjang_pend');
+		$this->db->order_by('nama_prodi', 'asc');
+		$query = $this->db->get('prodi');
+		return $query->result();
+	}
+	function input_form_3($data){
+		$this->db->insert('pilihan', $data);
+	}
+	public function provinsi(){
+		$this->db->where('id_level_wil', 1);
+		$query = $this->db->get('data_wilayah');
+		return $query->result();
+	}
+	public function getProv()
+	{
+	$this->db->order_by('nama','asc');
+    $query = $this->db->get('provinsi');
+    return $query->result();
+	}
+
+	public function getKab($id_prov)
+	{
+	  $this->db->where('id_prov', $id_prov);
+	  $this->db->order_by('nama','asc');
+		$query = $this->db->get('kabupaten');
+		return $query->result();
+	}
+	public function cekpilihan($id,$pilihan){
+		$this->db->where('id_mhs', $id);
+		$this->db->where('pilihan', $pilihan);
+		$query = $this->db->get('pilihan');
+		return $query->row();
+	}
+	public function jur_pil($id){
+		$this->db->join('prodi', 'prodi.id_prodi = pilihan.pilihan');
+		$this->db->join('jenjang_pend', 'jenjang_pend.id_jenjang_pend = prodi.id_jenjang_pend');
+		$this->db->where('id_mhs', $id);
+		$query = $this->db->get('pilihan');
+		return $query->result();
+	}
+	function edit_data_diri($id,$data){
+		$this->db->where('id_mhs',$id);
+		$this->db->update('calon_mhs', $data);
+	}
+	function edit_input_form_3($id,$data){
+		$this->db->where('id_mhs',$id);
+		$this->db->update('pilihan', $data);
+	}
+	 public function jumlah_pilihan($id){
+	 $this->db->where('id_mhs', $id);
+	 $query = $this->db->get('pilihan');
+	 return $query->num_rows();
+	}
+	function hapus_pilihan($id){
+		$this->db->where('id_pilihan', $id);
+		$this->db->delete('pilihan');
+	}
+	public function provbyid($id){
+		$this->db->where('id_prov', $id);
+		$query = $this->db->get('provinsi');
+		return $query->row();
+	}
+	public function detail_agama($id){
+		$this->db->where('id_agama', $id);
+		$query = $this->db->get('agama');
+		return $query->row();
+	}
+	public function kotabyid($id){
+		$this->db->where('id_kab', $id);
+		$query = $this->db->get('kabupaten');
+		return $query->row();
 	}
 }
